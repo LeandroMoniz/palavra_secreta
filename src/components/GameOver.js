14db69151e032579a7ceb76c6766f0b97594dee2
leandromoniz <leandromoniz55@gmail.com>
2023-04-28 11:39:22 -0300

    continuação do desenvolvimento, telas games

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -3,7 +3,7 @@ import "./GameOver.css"
 export const GameOver = ({retry}) => {
   return (
     <div>
-        <h1>Game Over</h1>
+        <h1>Fim do jogo!</h1>
         <button onClick={retry}>Resetar jogo</button>
     </div>
   )
